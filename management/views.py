093d212fd15771690c9de0e3243926306d5f775c
mocamocaland <noi_kaas@yahoo.co.jp>
2018-02-15 20:09:42 +0900

    login.html,views.pyをそれぞれ編集　ログイン部分のみ　途中まで

--- management/views.py
+++ management/views.py
@@ -1,3 +1,6 @@
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
-# Create your views here.
+
+#@login_required
