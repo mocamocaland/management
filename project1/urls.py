04ef904690a4305e3d8328136e62850be73eed32
mocamoca <noi_kaas@yahoo.co.jp>
2018-02-20 17:50:22 +0900

    ログイン認証画面のところを一旦コメントアウト

--- project1/urls.py
+++ project1/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
-from django.contrib.auth.views import login, logout
+#from django.contrib.auth.views import login, logout
 
 
 urlpatterns = [
